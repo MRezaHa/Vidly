8dffb4e0470fdc8e1604865b0895df96deed86a4
MRezaHa <m.reza.harati@gmail.com>
2018-07-30 12:28:45 +0430

    Exercise_Implementing_Validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,18 +7,24 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
 
+        [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
+        [Required]
         [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
 
         public DateTime? AddedDate { get; set; }
 
         [Display(Name = "Number In Stock")]
+        [Required]
+        [Range(1,20)]
         public int NumberInStock { get; set; }
 
 
